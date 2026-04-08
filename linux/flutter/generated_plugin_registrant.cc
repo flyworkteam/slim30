d084b79fa480c5b49d60d983ee9235be61e4fa6b
mratsag <mrat.sag@hotmail.com>
2026-04-08 21:06:54 +0300

    fix: include flutter_linux header in generated_plugin_registrant.cc

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_linux/flutter_linux.h>
 #include <file_selector_linux/file_selector_plugin.h>
 #include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
